2e6a7752018bdc7103ffae77a1377370c4a1ac67
dzsobacsi <tberki77@gmail.com>
2020-11-03 20:50:33 +0100

    ex_8.7

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -112,6 +112,10 @@ const typeDefs = gql`
       author: String!
       genres: [String]
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -144,6 +148,15 @@ const resolvers = {
         authors = [...authors, newAuthor]
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
